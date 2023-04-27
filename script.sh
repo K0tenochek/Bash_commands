d3a8ff962bbee3cf6a83ae38e6044fee41360d53
Tatiana <92399386+K0tenochek@users.noreply.github.com>
2023-04-27 23:58:27 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 mkdir dinner;
 cd dinner;
 mkdir chicken meat cheese;
@@ -5,4 +6,4 @@ cd meat;
 touch 1.txt 2.txt 3.txt 4.json 5.json;
 mkdir tea coffee juce;
 ls tea;
-mv 4.json 5.json tea;
+mv 4.json 5.json tea
